ea6326c272d4c0ef1a5292e85020c1451654de49
madina_afrolicious <30729081+Magda-dev@users.noreply.github.com>
2023-11-07 11:09:08 +0100

    UPDATE Css

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,35 +4,31 @@ let currencies = {'EUR': {'YEN': 0.0062,'GBP': 0.866600, 'USD': 1.07284},
                   'YEN': {'GBP': 184.813,'EUR': 160.168,'USD': 149.284}
                 }
 
-const dropList = document.querySelectorAll('form select');
 const convertBtn = document.querySelector('.submit');
-let fromSelect;
-let toSelect;
-let fromInput;    
+let fromSelect = document.querySelector('[name="from_currency"]').value;
+let toSelect = document.querySelector('[name="to_currency"]').value;
+let fromInput = document.querySelector('[name="from_amount"]').value;     
 let result = document.querySelector('.exchange-result');
 let rate = document.querySelector('.exchange-rate');
 
 convertBtn.addEventListener("click", (e)=>{
+    // clearing previous results 
     clear(rate);
     clear(result);
     e.preventDefault();
     let amount = document.querySelector("form input").value;
-    fromSelect = document.querySelector('[name="from_currency"]').value;
-    toSelect = document.querySelector('[name="to_currency"]').value;
-    fromInput = document.querySelector('[name="from_amount"]').value; 
+    // fromSelect = document.querySelector('[name="from_currency"]').value;
+    // toSelect = document.querySelector('[name="to_currency"]').value;
+    // fromInput = document.querySelector('[name="from_amount"]').value; 
     parseFloat(fromInput);
     parseFloat(fromSelect);
     parseFloat(toSelect);
     parseFloat(amount)
-    // if user don't enter any value or enter 0 then we'll put 1 value by default in the input field
-    a=fromSelect.value
-    // b=a.slice(0,3)
-    h=toSelect.value
-    // o=h.slice(0,3)
+  
     console.log(amount);
     console.log(fromSelect.value);
     console.log(toSelect.value);
-    console.log(currencies[fromSelect][toSelect]);
+    // console.log(currencies[fromSelect][toSelect]);
    
     
     
@@ -42,7 +38,7 @@ convertBtn.addEventListener("click", (e)=>{
                 rate.innerHTML = 'Veuillez entrer un montant !';
                 rate.style.color = 'hsl(357, 92%, 47%)';
              }else{
-           result.innerHTML = (amount * currencies[fromSelect][toSelect]).toFixed(2);
+           result.innerHTML = (amount * currencies[fromSelect][toSelect]).toFixed(3);
            rate.innerHTML = 'Taux de change : ' + currencies[fromSelect][toSelect];
            rate.style.color = 'hsl(0, 0%, 100%)';
  
@@ -52,12 +48,12 @@ convertBtn.addEventListener("click", (e)=>{
 
 
 const swapIcon = document.querySelector(".icon");
-swapIcon.addEventListener("click", (e)=>{
-    e.preventDefault();
+swapIcon.addEventListener("click", ()=>{
+    // e.preventDefault();
 
-    fromSelect = document.querySelector('[name="from_currency"]').value;
-    toSelect = document.querySelector('[name="to_currency"]').value;
-    fromInput = document.querySelector('[name="from_amount"]').value; 
+    // fromSelect = document.querySelector('[name="from_currency"]').value;
+    // toSelect = document.querySelector('[name="to_currency"]').value;
+    // fromInput = document.querySelector('[name="from_amount"]').value; 
     parseFloat(fromInput);
     parseFloat(fromSelect);
     parseFloat(toSelect);
@@ -66,7 +62,7 @@ swapIcon.addEventListener("click", (e)=>{
     let tempCurrency = fromSelect; // temporary currency code of FROM drop list
     fromSelect = toSelect; // passing TO currency code to FROM currency code
     toSelect = tempCurrency; // passing temporary currency code to TO currency code
-    // getExchangeRate(); // calling getExchangeRate
+    
     console.log(fromSelect.value);
     console.log(toSelect.value);
 })
